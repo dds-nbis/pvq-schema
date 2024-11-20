2e020edfa038b155cd1eef2c347598a8387c9f16
greg-vtg <gregory.o.steffensen@odni.gov>
2024-11-20 10:15:28 -0500

    Removing dummy content from part A overrides.

--- schema-generation/parser/part-a-overrides.js
+++ schema-generation/parser/part-a-overrides.js
@@ -1,9 +1,2 @@
 window.pvqOverrides = {
-    "section_02": {
-        "n305": [{
-            "text": "Have you ever had a U.S. passport book?",
-            "datatype": "checkboxes",
-            "checkboxes": ["Yes", "No", "I don't know"]
-        }]
-    }
-}
+};
