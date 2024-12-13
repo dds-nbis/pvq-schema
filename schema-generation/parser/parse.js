3e5eefa536db5e8d7e7465632747ce25ec9a7efd
greg-vtg <gregory.o.steffensen@odni.gov>
2024-12-13 14:36:26 -0500

    Improvements to question ID algorithm.

--- schema-generation/parser/parse.js
+++ schema-generation/parser/parse.js
@@ -21,16 +21,7 @@ function getAbsolutePosition(element) {
   };
 }
 
-function createQuestionid(condition, text) {
-    const str = condition.trim() + "::" + text.trim();
-    let hash = 0;
-    for (let i = 0; i < str.length; i++) {
-        const char = str.charCodeAt(i);
-        hash = ((hash << 5) - hash) + char;
-        hash = hash & hash; // Convert to 32-bit integer
-    }
-    return Math.abs(hash).toString(16);
-}
+
 
 /**
  * Returns the substring in s after the first occurrence of d,
@@ -315,107 +306,80 @@ function cleanupNbspChunks(questionText, secondaryChunks) {
     }
 }
 
-function parseQuestions(allChunks) {
-    let leftEdge = 10000;
-    let leftmostChunks = [];
-    let otherChunks = [];
-    let filteredChunks = [];
-    for (const chunk of allChunks) {
-        // skip all chunks that aren't in the Garamond font
-        let styles = getEffectiveStyle(chunk);
-        let fontFamily = styles.fontFamily;
-        if (fontFamily.indexOf("Garamond") < 0) {
-            continue;
-        }
-        filteredChunks.push(chunk);
-        let left = getXCoord(chunk);
-        if (left < leftEdge) {
-            leftEdge = left;
-        }
+/**
+ * A simple, fast string hash algorithm, taken from
+ * 
+ * https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
+ */
+function hashString(str, seed = 48193) {
+    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+    for(let i = 0, ch; i < str.length; i++) {
+        ch = str.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
     }
+    h1  = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+    h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2  = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+    h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+  
+    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+};
 
+const QUESTION_HASH_COUNTER = new Map();
 
-    for (const chunk of filteredChunks) {
-        let left = getXCoord(chunk);
-        let deltaX = left - leftEdge;
-        if (deltaX < 20) {
-            leftmostChunks.push(chunk);
-        } else {
-            otherChunks.push(chunk);
-        }
-    }
+/**
+ * Creates a parser question ID from the PVQ part, the section number, the current condition
+ * text and the question text.
+ * 
+ * The returned ID has 4 parts, separated by hyphens:
+ * 
+ * - The PVQ part (a single lowercase letter)
+ * - The section number (a positive integer)
+ * - The question hash
+ * - A counter (a non-negative integer)
+ * 
+ * The PVQ part and section number are passed in directly. The question hash is the first
+ * 6 characters of hash string calculated for the combination of the condition text and 
+ * question text. The counter is the number of times that question hash has been used before
+ * (in any PVQ part or section, though I don't expect to find the same combination of condition
+ * text and question text in different sections). The counter is necessary because there is
+ * at least 1 case where the exact same question text is used twice within the same section 
+ * and conditional logic group. The counter starts at 0, so for the first question with a 
+ * particular condition and text, the counter will be 0, for the second it will be 1, etc.
+ * The counter also protects against hash collisions.
+ * 
+ * This approach should provide reasonable resilience to most of the changes we expect
+ * the PVQ documents to have. For example:
+ * 
+ * - Adding a new question won't change the ID of any other questions (except in the
+ *   rare case that the question text and branch condition are identical to a previous
+ *   question)
+ * - Deleting a question won't change the ID of any other question (except in the
+ *   rare case that the question ID and branch conditoin are identical to a subsequent
+ *   question)
+ * - Modifying the text of a question changes its ID, but no other question IDs
+ * - Modifying the text of a branch condition changes the IDs of questions within
+ *   that branch, but no others.
+ * - Reordering questions and instructions doesn't change any of the question IDs 
+ *   (except in the rare case that two of them share the same question text and 
+ *   branch condition)
+ * - Changing the formatting of questions or other document content (i.e. whether a 
+ *   question is in a table or a para tag) doesn't change their IDs.
+ * 
+ * This should make it easier to update the JSON schema due to new PVQ drafts
+ * without a ton of work.
+ */
+function createQuestionId(pvqPart, sectionNum, conditionText, questionText) {
+    conditionText = cleanText(conditionText);
+    questionText = cleanText(questionText);
+    const hash = hashString(`${conditionText}:::${questionText}`);
+    const hashPrefix = Math.abs(hash).toString(16).slice(0,6);
 
-    sortByPosition(leftmostChunks);
-    sortByPosition(otherChunks);
-
-    let primaryChunks = [];
-    let prevBottom = 0;
-    // some of the questions are formatted as a single logical paragraph broken into multiple 
-    // lines by <p> tags. When this happens, the bottom of one <p> box is the top of the next 
-    // <p> box, so we can use y-axis gaps to reassemble them into one text string. When this 
-    // happens, we store the text of all but the first chunk in a custom attribute on the 
-    // first chunk's DOM element.
-    for (const chunk of leftmostChunks) {
-        const text = cleanText(chunk);
-        let box = getAbsolutePosition(chunk);
-        let yGap = box.top - prevBottom;
-        if (yGap < 4) {
-            let curQuestionChunk = primaryChunks.at(-1);
-            let curSuffix = curQuestionChunk.getAttribute("question-suffix") || "";
-            curQuestionChunk.setAttribute("question-suffix", curSuffix + "\n" + text);
-        } else {
-            primaryChunks.push(chunk);
-        }
-        prevBottom = box.bottom;
-    }
-
-    let reorganized = new Map();
-    for (const e of primaryChunks) {
-        reorganized.set(e, []);
-    }
-
-    if (primaryChunks.length == 1) {
-        // Usually, we try to match right side secondary question chunks with left side primary question chunks
-        // by finding the lowest left side chunk whose y position is equal to or above the right side chunk.
-        // But there is one edge question in part A where the checkbox elements are slightly above the 
-        // matching question text on the left, just because of bad formatting. That's the only 
-        // question in that question group, so this is a small hack that allows us to match up the 
-        // checkbox chunks with their question text chunk in that case
-        const primaryChunk = primaryChunks[0];
-        otherChunks
-            .map(e => cleanText(e))
-            .forEach(s => {
-                reorganized.get(primaryChunk).push(s);
-            }); 
-    } else {
-        for (const e of otherChunks) {
-            let contextQuestion = findFirstAbove(e, primaryChunks);
-            const text = cleanText(e);
-            if (contextQuestion != null) {
-                reorganized.get(contextQuestion).push(text);
-            } else {
-                console.warn("Couldn't find context question for %o", e);
-            }
-        }
-    }
-    let output = [];
-    for (let e of reorganized.keys()) {
-        const mainText = e.innerText;
-        const questionId = e.getAttribute("pvq-id") || "";
-        console.assert(questionId.length > 0, "no PVQ ID found on primary chunk: %o", e);
-        const suffix = e.getAttribute("question-suffix") || "";
-        const examineHints = [];
-        if (suffix != "") {
-            examineHints.push("complex_text_dom");
-            console.warn("Assembling multi-chunk question text e=%o, main='%s' suffix='%s'", e, mainText, suffix);
-        }
-        const questionText = (mainText + " " + suffix).trim();
-        let secondaryChunks = reorganized.get(e);
-        const cleaned = cleanupNbspChunks(questionText, secondaryChunks);
-        const question = Question.create(questionId, cleaned.questionText, cleaned.secondaryChunks, examineHints);
-        output.push(question);
-    }
-    return output;
+    const curCount = QUESTION_HASH_COUNTER.get(hashPrefix) || 0;
+    const qId = `${pvqPart}-${sectionNum}-${hashPrefix}-${curCount}`;
+    QUESTION_HASH_COUNTER.set(hashPrefix, curCount + 1);
+    return qId;
 }
 
 class QuestionGroupParser {
@@ -426,16 +390,20 @@ class QuestionGroupParser {
         "questionChunks": [],
     };
 
+    #conditionCounters = new Map();
+
     groups = [];
 
-    constructor(sectionOverrides) {
+    constructor(pvqPart, sectionNum, sectionOverrides) {
+        this.pvqPart = pvqPart;
+        this.sectionNum = sectionNum;
         this.sectionOverrides = sectionOverrides || {};
     }
 
     endGroup() {
         if (this.#currentGroup.questionChunks.length > 0) {
             sortByPosition(this.#currentGroup.questionChunks);
-            const questions = parseQuestions(this.#currentGroup.questionChunks);
+            const questions = this.#parseQuestions(this.#currentGroup.condition, this.#currentGroup.questionChunks);
             processOverrides(questions, this.sectionOverrides);
             const joinedPrologue = this.#currentGroup.prologueContent.join("\n\n");
             this.groups.push({
@@ -453,6 +421,14 @@ class QuestionGroupParser {
         };
     }
 
+    startGroup(conditionText) {
+        const normalizedCondition = substringAfter("branch auto populate", cleanText(conditionText), true).trim();
+        this.#currentCondition = normalizedCondition;
+        const curCount = this.#conditionCounters.get(normalizedCondition) || 0;
+        this.#conditionCounters.set(normalizedCondition, curCount + 1);
+        this.endGroup();
+    }
+
     addContent(e) {
         const text = cleanText(e);
         if (text.length == 0) {
@@ -461,8 +437,7 @@ class QuestionGroupParser {
 
         const contentType = detectContentType(e);
         if (contentType == "branch-start") {
-            this.#currentCondition = substringAfter("branch auto populate", cleanText(e), true).trim();
-            this.endGroup();
+            this.startGroup(cleanText(e));
         } else if (contentType == "branch-end" || contentType == "heading") {
             this.#currentCondition = "";
             this.endGroup();
@@ -476,6 +451,108 @@ class QuestionGroupParser {
         } else {
             throw "unexpected content type: " + contentType;
         }
+    }
+
+    #parseQuestions(conditionText, allChunks) {
+        let leftEdge = 10000;
+        let leftmostChunks = [];
+        let otherChunks = [];
+        let filteredChunks = [];
+        for (const chunk of allChunks) {
+            // skip all chunks that aren't in the Garamond font
+            let styles = getEffectiveStyle(chunk);
+            let fontFamily = styles.fontFamily;
+            if (fontFamily.indexOf("Garamond") < 0) {
+                continue;
+            }
+            filteredChunks.push(chunk);
+            let left = getXCoord(chunk);
+            if (left < leftEdge) {
+                leftEdge = left;
+            }
+        }
+
+
+        for (const chunk of filteredChunks) {
+            let left = getXCoord(chunk);
+            let deltaX = left - leftEdge;
+            if (deltaX < 20) {
+                leftmostChunks.push(chunk);
+            } else {
+                otherChunks.push(chunk);
+            }
+        }
+
+        sortByPosition(leftmostChunks);
+        sortByPosition(otherChunks);
+
+        let primaryChunks = [];
+        let prevBottom = 0;
+        // some of the questions are formatted as a single logical paragraph broken into multiple 
+        // lines by <p> tags. When this happens, the bottom of one <p> box is the top of the next 
+        // <p> box, so we can use y-axis gaps to reassemble them into one text string. When this 
+        // happens, we store the text of all but the first chunk in a custom attribute on the 
+        // first chunk's DOM element.
+        for (const chunk of leftmostChunks) {
+            const text = cleanText(chunk);
+            let box = getAbsolutePosition(chunk);
+            let yGap = box.top - prevBottom;
+            if (yGap < 4) {
+                let curQuestionChunk = primaryChunks.at(-1);
+                let curSuffix = curQuestionChunk.getAttribute("question-suffix") || "";
+                curQuestionChunk.setAttribute("question-suffix", curSuffix + "\n" + text);
+            } else {
+                primaryChunks.push(chunk);
+            }
+            prevBottom = box.bottom;
+        }
+
+        let reorganized = new Map();
+        for (const e of primaryChunks) {
+            reorganized.set(e, []);
+        }
+
+        if (primaryChunks.length == 1) {
+            // Usually, we try to match right side secondary question chunks with left side primary question chunks
+            // by finding the lowest left side chunk whose y position is equal to or above the right side chunk.
+            // But there is one edge question in part A where the checkbox elements are slightly above the 
+            // matching question text on the left, just because of bad formatting. That's the only 
+            // question in that question group, so this is a small hack that allows us to match up the 
+            // checkbox chunks with their question text chunk in that case
+            const primaryChunk = primaryChunks[0];
+            otherChunks
+                .map(e => cleanText(e))
+                .forEach(s => {
+                    reorganized.get(primaryChunk).push(s);
+                });
+        } else {
+            for (const e of otherChunks) {
+                let contextQuestion = findFirstAbove(e, primaryChunks);
+                const text = cleanText(e);
+                if (contextQuestion != null) {
+                    reorganized.get(contextQuestion).push(text);
+                } else {
+                    console.warn("Couldn't find context question for %o", e);
+                }
+            }
+        }
+        let output = [];
+        for (let e of reorganized.keys()) {
+            const mainText = e.innerText;
+            const suffix = e.getAttribute("question-suffix") || "";
+            const examineHints = [];
+            if (suffix != "") {
+                examineHints.push("complex_text_dom");
+                console.warn("Assembling multi-chunk question text e=%o, main='%s' suffix='%s'", e, mainText, suffix);
+            }
+            const questionText = (mainText + " " + suffix).trim();
+            let secondaryChunks = reorganized.get(e);
+            const cleaned = cleanupNbspChunks(questionText, secondaryChunks);
+            const questionId = createQuestionId(this.pvqPart, this.sectionNum, conditionText, questionText);
+            const question = Question.create(questionId, cleaned.questionText, cleaned.secondaryChunks, examineHints);
+            output.push(question);
+        }
+        return output;
     }
 }
 
@@ -512,8 +589,8 @@ function processOverrides(questions, overrides) {
     questions.push(...newQuestions);
 }
 
-function parseQuestionGroups(sectionContent, sectionOverrides) {
-    const parser = new QuestionGroupParser(sectionOverrides);
+function parseSection(pvqPart, sectionNum, sectionContent, sectionOverrides) {
+    const parser = new QuestionGroupParser(pvqPart, sectionNum, sectionOverrides);
     for (const e of sectionContent) {
         parser.addContent(e);
     }
@@ -637,7 +714,7 @@ function parseDoc(config) {
         const sectionId = `section_${sectionNum.toString().padStart(2, '0')}`;
         const content = nodesBySection.get(sectionHeading);
         console.groupCollapsed("Parsing section: %s", sectionHeading);
-        const groups = parseQuestionGroups(content, overrides);
+        const groups = parseSection(pvqPart, sectionNum, content, overrides);
         console.groupEnd();
         parsedSections[sectionId] = {
             "name": substringAfter("Section", sectionHeading).trim(),
@@ -648,6 +725,14 @@ function parseDoc(config) {
     const allGroups = Object.values(parsedSections)
         .flatMap(s => s.groups);
     const allQuestions = allGroups.flatMap(g => g.questions);
+    const allIds = allQuestions.map(q => q.id);
+    for (let i = 0; i < allIds.length; i++) {
+        const qId = allIds[i];
+        var firstIndex = allIds.indexOf(qId);
+        if (i !== firstIndex) {
+            console.warn("DUPLICATE ID questionId=%s i=%s firstIndex=%s", qId, typeof i, typeof firstIndex);
+        }
+    }
 
     const typeCounts = {};
     allQuestions.map(q => q.dataType)
