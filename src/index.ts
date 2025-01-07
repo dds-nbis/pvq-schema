d1eb4ba3216016f6e1e896586804be2d22228b96
David Saeger <david@dds.mil>
2025-01-07 09:46:12 -0500

    wip on education

--- src/index.ts
+++ src/index.ts
@@ -326,7 +326,54 @@ export const PersonWhoKnowsYou = Type.Object({
   emailAddress: Type.Array(EmailAddress),
   address: Type.Union([USAddress, NonUsAddress])
 })
-
+export const EducationalInstitution = Type.Object({ 
+  name: Type.String(),
+  type: Type.Union([Type.Literal("trade"), Type.Literal("higher"), Type.Literal("secondary"), Type.Literal("other")]),
+  learningExperience: Type.Union([Type.Literal("inPerson"), Type.Literal("distance"), Type.Literal("hybrid")]),
+  datesAttended: DateRange,
+  degreeReceived: Type.Union([Type.Literal('yes'), Type.Literal('no')]),
+  physicalLocation: Type.Union([Type.Literal("yes"), Type.Literal("no")]),
+  schoolAddress: Type.Object({
+    usBased: Type.Boolean(),
+    street: Type.Optional(Type.String()),
+    city: Type.String(),
+    country: Type.Optional(Type.String()),
+    stateOrTerritory: Type.Optional(Type.String()),
+    zipCode: Type.Optional(Type.String()),
+  }),
+  physicalAddress: Type.Object({
+    usBased: Type.Boolean(),
+    street: Type.Optional(Type.String()),
+    city: Type.String(),
+    country: Type.Optional(Type.String()),
+    stateOrTerritory: Type.Optional(Type.String()),
+    zipCode: Type.Optional(Type.String()),
+  }),
+  degree: Type.Optional(Type.Object({
+    type: Type.Union([Type.Literal('associates'), Type.Literal('bachelors'), Type.Literal('masters'), Type.Literal('professional'),Type.Literal('doctorate'),Type.Literal('highSchool'), Type.Literal('other')]),
+    date: Type.String(),
+    dateEstimated: Type.Boolean(),
+  })),
+  reference: FullName,
+  referenceRelation: Type.Union([Type.Literal("instructor"),Type.Literal("counselor"), Type.Literal("friend"), Type.Literal("schoolmate"), Type.Literal("other")]),
+  referenceRelationOther: Type.Optional(Type.String()),
+  referencePhoneNumber: Type.Array(Type.Union([PhoneNumber, Type.Object({
+    dsn: Type.Union([Type.Literal("yes"), Type.Literal("no")]),
+    sensitivePermission: Type.Union([Type.Literal("yes"), Type.Literal("no")]),
+  })])),
+  referenceLiveOrWorkInUs: Type.Union([Type.Literal("yes"), Type.Literal("no")]),
+  referenceAddress: Type.Object({
+    usBased: Type.Boolean(),
+    street: Type.Optional(Type.String()),
+    city: Type.String(),
+    country: Type.Optional(Type.String()),
+    stateOrTerritory: Type.Optional(Type.String()),
+    zipCode: Type.Optional(Type.String()),
+    isUsMilitaryInstallation: Type.Union([Type.Literal("yes"), Type.Literal("no")]),
+    usMilitaryInstallationName: Type.Optional(Type.String()),
+    usMilitaryInstallationZip: Type.Optional(Type.String())
+  }),
+  })
 export const PVQSchema = Type.Object({
   version: Type.Number(),
   // Section 01
